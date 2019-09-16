58f1f47133fd0cfc30adc22c8cb5ec82a0dbc04a
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-16 23:44:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  p hash.key
 end
 
 def update_counting_hash(hash, key)
